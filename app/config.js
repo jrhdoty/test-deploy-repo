2dbacbbe96968f6e69ec92c4977b80fc746fb795
Hack Reactor Students <accounts@hackreactor.com>
2014-05-15 17:10:00 -0700

    Uses mongoDB for user login/signup

--- app/config.js
+++ app/config.js
@@ -1,6 +1,18 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-// var path = require('path');
+var bcrypt = require('bcrypt-nodejs');
+var Promise = require('bluebird');
+
+var hashPassword = function(pw){
+  //return hashed password
+    var cipher = Promise.promisify(bcrypt.hash);
+
+    return cipher(pw, null, null)
+        .then(function(hash) {
+          console.log('hash is: ', hash);
+          return hash;
+        });
+  };
 
 mongoose.connect('mongodb://localhost/curtlyDB');
 
@@ -17,63 +29,85 @@ db.urlSchema = new Schema({
 
 db.usersSchema = new Schema({
   username: {type: String, unique: true},
-  password: String
+  password: {type: String}
+});
+
+db.usersSchema.pre('save', function(next){
+  var cipher = Promise.promisify(bcrypt.hash);
+  return cipher(this.password, null, null).bind(this)
+  .then(function(hash){
+    this.password = hash;
+    next();
+  });
 });
 
 db.User = mongoose.model('User', db.usersSchema);
 
+var promisifiedSave = Promise.promisify((new db.User).save);
+
 db.Url =  mongoose.model('Url', db.urlSchema);
 
-/*   example usage
-var user = new db.User({username: "hello", password: "world"});
-user.save(function(err){
-  if (err){
-    console.error(err);
+// //   example usage
+// var user = new db.User({username: "robesfrto41234", password: "6"});
+// user.save(function(err){
+//   if (err){
+//     console.error(err);
+//   } else {
+//     console.log("user inserted successfully");
+//   }
+// });
+//
+  // var query = db.User.find({username : 'roberto4asdf'});
+  // query.exec(function(err, results){
+  //   console.log(results);
+  // });
+
+// var username = 'rro4berto4aasdsdffsdf';
+// var password = 'this is a password';
+// var query = db.User.find({username : username});
+// console.log('query returned');
+// query.exec().then(function(results){
+//   console.log("callback from then");
+//   console.log(results);
+//   console.log(results.length);
+//   if(results.length === 0){
+//     var newUser = new db.User({
+//       username: username,
+//       password: password
+//     });
+//     return promisifiedSave.call(newUser);
+//   } console.log("user already existed");
+// }, function(err){
+//   console.log(err);
+//   throw err;
+// }).then(function(value){
+//   console.log("final value is: ", value);
+//   console.log('promisifying worked');
+//   // redirect ?
+// });
+//
+var username = 'rro4berto4aasdsdffsdf';
+var password = 'this is a password!!';
+var promisifiedCompare = Promise.promisify(bcrypt.compare);
+
+var query = db.User.find({username : username});
+query.exec().then(function(results){
+  if(results.length === 0){
+    //res.redirect('/login');
   } else {
-    console.log("user inserted successfully");
+    //check if passwords match
+    var hashedPassword = results[0].password;
+    return promisifiedCompare(password, hashedPassword);
+  }
+}, function(err){
+  throw err;
+}).then(function(match){
+  if (match) {
+    console.log('passwords match')
+  } else {
+    console.log('no match')
   }
 });
-*/
-
-// var db = Bookshelf.initialize({
-//   client: 'sqlite3',
-//   connection: {
-//     host: '127.0.0.1',
-//     user: 'your_database_user',
-//     password: 'password',
-//     database: 'shortlydb',
-//     charset: 'utf8',
-//     filename: path.join(__dirname, '../db/shortly.sqlite')
-//   }
-// });
-// db.knex.schema.hasTable('users').then(function(exists) {
-//   if (!exists) {
-//     db.knex.schema.createTable('users', function (user) {
-//       user.increments('id').primary();
-//       user.string('username', 100).unique();
-//       user.string('password', 100);
-//       user.timestamps();
-//     }).then(function (table) {
-//       console.log('Created Table', table);
-//     });
-//   }
-// });
-
-// db.knex.schema.hasTable('urls').then(function(exists) {
-//   if (!exists) {
-//     db.knex.schema.createTable('urls', function (link) {
-//       link.increments('id').primary();
-//       link.string('url', 255);
-//       link.string('base_url', 255);
-//       link.string('code', 100);
-//       link.string('title', 255);
-//       link.integer('visits');
-//       link.timestamps();
-//     }).then(function (table) {
-//       console.log('Created Table', table);
-//     });
-//   }
-// });
-
+console.log('on the other side of the promise');
 
 module.exports = db;
