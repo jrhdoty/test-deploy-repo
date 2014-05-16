43aec14843f730342e20efe279b2a2f574817e2e
Hack Reactor Students <accounts@hackreactor.com>
2014-05-15 21:06:50 -0700

    Implements full functionality with mongoDB

--- app/config.js
+++ app/config.js
@@ -27,6 +27,15 @@ db.urlSchema = new Schema({
   visits: Number
 });
 
+db.urlSchema.pre('save', function(next){
+  var cipher = Promise.promisify(bcrypt.hash);
+  return cipher(this.url, null, null).bind(this)
+  .then(function(hash){
+    this.code = hash.slice(15, 22);
+    next();
+  });
+});
+
 db.usersSchema = new Schema({
   username: {type: String, unique: true},
   password: {type: String}
@@ -43,71 +52,32 @@ db.usersSchema.pre('save', function(next){
 
 db.User = mongoose.model('User', db.usersSchema);
 
-var promisifiedSave = Promise.promisify((new db.User).save);
+// var promisifiedSave = Promise.promisify((new db.User).save);
 
 db.Url =  mongoose.model('Url', db.urlSchema);
 
-// //   example usage
-// var user = new db.User({username: "robesfrto41234", password: "6"});
-// user.save(function(err){
-//   if (err){
-//     console.error(err);
+// var username = 'rro4berto4aasdsdffsdf';
+// var password = 'this is a password!!';
+// var promisifiedCompare = Promise.promisify(bcrypt.compare);
+
+// var query = db.User.find({username : username});
+// query.exec().then(function(results){
+//   if(results.length === 0){
+//     //res.redirect('/login');
 //   } else {
-//     console.log("user inserted successfully");
+//     //check if passwords match
+//     var hashedPassword = results[0].password;
+//     return promisifiedCompare(password, hashedPassword);
+//   }
+// }, function(err){
+//   throw err;
+// }).then(function(match){
+//   if (match) {
+//     console.log('passwords match')
+//   } else {
+//     console.log('no match')
 //   }
 // });
-//
-  // var query = db.User.find({username : 'roberto4asdf'});
-  // query.exec(function(err, results){
-  //   console.log(results);
-  // });
-
-// var username = 'rro4berto4aasdsdffsdf';
-// var password = 'this is a password';
-// var query = db.User.find({username : username});
-// console.log('query returned');
-// query.exec().then(function(results){
-//   console.log("callback from then");
-//   console.log(results);
-//   console.log(results.length);
-//   if(results.length === 0){
-//     var newUser = new db.User({
-//       username: username,
-//       password: password
-//     });
-//     return promisifiedSave.call(newUser);
-//   } console.log("user already existed");
-// }, function(err){
-//   console.log(err);
-//   throw err;
-// }).then(function(value){
-//   console.log("final value is: ", value);
-//   console.log('promisifying worked');
-//   // redirect ?
-// });
-//
-var username = 'rro4berto4aasdsdffsdf';
-var password = 'this is a password!!';
-var promisifiedCompare = Promise.promisify(bcrypt.compare);
-
-var query = db.User.find({username : username});
-query.exec().then(function(results){
-  if(results.length === 0){
-    //res.redirect('/login');
-  } else {
-    //check if passwords match
-    var hashedPassword = results[0].password;
-    return promisifiedCompare(password, hashedPassword);
-  }
-}, function(err){
-  throw err;
-}).then(function(match){
-  if (match) {
-    console.log('passwords match')
-  } else {
-    console.log('no match')
-  }
-});
-console.log('on the other side of the promise');
+// console.log('on the other side of the promise');
 
 module.exports = db;
